ce28a76630882614e0c4f42864299bcfdfa66184
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-07-01 00:04:57 +0200

    Part 5 - Set up Fire API and all grid stuff

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -12,11 +12,37 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Launcher {
+    public void displayGrid(GameGrid gameGrid) {
+        System.out.print(String.format("\033[H\033[2J"));
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++)
+            {
+                Ship elmt = gameGrid.get_grid()[i][j];
+                if (elmt == null) {
+                    System.out.print(".  |  ");
+                }
+                else if (elmt.get_slug().equals("hit")) {
+                    System.out.print("\u001B[31m" + "X" + "\u001B[0m");
+                    System.out.print("  |  ");
+                    gameGrid.get_grid()[i][j] = null;
+                }
+                else {
+                    System.out.print(elmt.get_slug().toUpperCase().charAt(0));
+                    System.out.print("  |  ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
     public HttpServer startServer(int port) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         ExecutorService executor = Executors.newFixedThreadPool(1);
+        GameGrid gameGrid = new GameGrid(10, 10);
+        displayGrid(gameGrid);
         server.createContext("/ping", new Ping());
         server.createContext("/api/game/start", new StartGame());
+        server.createContext("/api/game/fire", new Fire(gameGrid));
         server.setExecutor(executor);
         server.start();
 
