ae287d6385c9c0848b5d0026f2a7f0886767b46c
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-06-30 23:59:08 +0200

    Part 4 - Remove unused imports

--- src/main/java/fr/lernejo/navy_battle/Ping.java
+++ src/main/java/fr/lernejo/navy_battle/Ping.java
@@ -4,13 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.OutputStream;
-import java.io.InputStream;
 import java.io.IOException;
-
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.loader.SchemaLoader;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 
 class Ping implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
