2b91e1e1d56789e2bbe8bd660acbf818bf6bf664
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-07-05 17:48:30 +0200

    Add again ping test and launcher test

---
+++ src/test/java/fr/lernejo/navy_battle/PingTest.java
@@ -0,0 +1,30 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+class PingTest {
+    @Test
+    void ping_test_prints_hello() throws Exception {
+        int port = 9876;
+        StartStopServer startStopServer = new StartStopServer();
+        HttpServer server = startStopServer.startServer(port);
+
+        ProcessBuilder pb = new ProcessBuilder("curl", "http://localhost:9876/ping");
+        Process p = pb.start();
+
+        InputStream is = p.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        Assertions.assertEquals("OK", br.readLine());
+
+        p.destroy();
+        startStopServer.stopServer(server);
+    }
+}
